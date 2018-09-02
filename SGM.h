20c779049b060aa8d99f103b3ca599cdb56437ed
SanYu Huang <san.huang.ext@siemens.com>
2018-09-02 21:50:34 +0200

    clean up

--- SGM.h
+++ SGM.h
@@ -1,5 +1,10 @@
-#ifndef _SGM_H_
-#define _SGM_H_
+/**
+ * \brief This file defines a simple SGM example based on oOpenCV library.
+ * \author      SanYu Huang (sanjuu11525@hotmail.com)
+ * \maintainer  SanYu Huang (sanjuu11525@hotmail.com)
+ * \copyright   There is no copy right. The file is recommended for education purpose.
+ */
+#pragma once
 
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
@@ -9,98 +14,87 @@
 #define DIS_FACTOR 1
 #define CENSUS_W 5
 #define DISFLAG 255
-#define Disthreshold 10000
-#define Outlier 255
 #define Vmax 0.7
 #define disparityThreshold 2
 #define Dinvd 0
 #define visualDisparity 1
-using namespace cv;
-
-class SGM
-{
-
-	protected:
-		typedef Vec<float, DISP_RANGE> VecDf;
-		int PENALTY1;
-		int PENALTY2;
-		const int winRadius;
-		const cv::Mat &imgLeft; 
-		const cv::Mat &imgRight;
-		const cv::Mat &imgLeftLast;
-		cv::Mat censusImageRight;
-		cv::Mat censusImageLeft;
-		cv::Mat censusImageLeftLast;
-		cv::Mat cost;
-		cv::Mat costRight;
-		cv::Mat directCost;
-		cv::Mat accumulatedCost;
-		int HEIGHT;
-		int WIDTH;
-		std::vector<cv::Point2i> invalidDisparity; 
 
 
-		void computeCensus(const cv::Mat &image, cv::Mat &censusImg);
-		int  computeHammingDist(const uchar left, const uchar right);
-		VecDf addPenalty(VecDf const& prior, VecDf &local, float path_intensity_gradient);
-		void sumOverAllCost();
-		virtual void createDisparity(cv::Mat &disparity);
-		template <int DIRX, int DIRY> void aggregation(cv::Mat cost);
-		virtual void computeDerivative();
-		virtual void computeCost();
-		virtual void computeCostRight();
-		virtual void postProcess(cv::Mat &disparity);
-		virtual void resetDirAccumulatedCost();
-		virtual void resetDirAccumulatedCostLast();
-		virtual void consistencyCheck(cv::Mat disparityLeft, cv::Mat disparityRight, cv::Mat disparity, bool interpl);
-	public:
-		SGM(const cv::Mat &imgLeftLast_, const cv::Mat &imgLeft_, const cv::Mat &imgRight_, const int PENALTY1_, const int PENALTY2_, const int winRadius_);
-		void setPenalty(const int penalty_1, const int penalty_2);	
-		void runSGM(cv::Mat &disparity);
+namespace sgmflow {
 
-		virtual void writeDerivative();
-		virtual ~SGM();	
-	
+struct SGMStereoParameters {
+  int penalty1 = 400;
+  int penalty2 = 6000;
+  int win_radius = 2;
+  float sobel_cap_value = 15;
+  int height;
+  int width;
+};
+
+class SGM {
+public:
+  typedef cv::Vec<float, DISP_RANGE> vector_type;
+  typedef SGMStereoParameters parameter_type;
+
+protected:
+  const cv::Mat &img_left_;
+  const cv::Mat &img_right_;
+  const cv::Mat &img_left_last_;
+  cv::Mat census_image_right_;
+  cv::Mat census_image_left_;
+  cv::Mat census_image_left_last_;
+  cv::Mat cost_left_to_right_;
+  cv::Mat cost_right_to_left_;
+  cv::Mat accumulated_cost_;
+
+  void computeCensus(const cv::Mat &image, cv::Mat &censusImg);
+  int  computeHammingDist(const uchar left, const uchar right);
+  vector_type addPenalty(vector_type const& prior, vector_type &local);
+  void sumOverAllCost(cv::Mat& pathWiseCost);
+  virtual void createDisparity(cv::Mat &disparity);
+  template <int DIRX, int DIRY> void aggregation(cv::Mat cost);
+  virtual void computeDerivative() = 0;
+  virtual void computeCost() = 0;
+  virtual void computeCostRight() = 0;
+  virtual void consistencyCheck(cv::Mat disparityLeft, cv::Mat disparityRight, cv::Mat disparity, bool interpl) = 0;
+
+//  void aggregationCUDA(cv::Mat &directAcc, cv::Mat &cost);
+
+public:
+  SGM(const cv::Mat &imgLeftLast_, const cv::Mat &imgLeft_, const cv::Mat &imgRight_, parameter_type parameters);
+  void process(cv::Mat &disparity);
+  virtual ~SGM();
+
+protected:
+  parameter_type parameters_;
 };
 
 
+class SGMFlow : public SGM {
+protected:
+  cv::Mat img_rotation_;
+  cv::Mat img_rotation_backward_;
+  cv::Mat epipole_left_last_;
+  cv::Mat epipole_left_;
+  cv::Mat translation_left_last_;
+  cv::Mat translation_left_;
+  cv::Mat fundamental_matrix_;
+  cv::Mat derivative_flow_left_last_;
+  cv::Mat derivative_flow_left_;
 
-class SGMFlow : public SGM
-{
+  virtual void createDisparity(cv::Mat &disparity);
+  virtual void computeDerivative();
+  virtual void computeCost();
+  virtual void computeCostRight();
+  void computeRotation();
+  void computeBackwardRotation();
+  void computeTranslation(cv::Mat &translation, cv::Mat &Epipole, float sign);
+  virtual void consistencyCheck(cv::Mat disparityLeft, cv::Mat disparityRight, cv::Mat disparity, bool interpl);
 
-	protected:
-		cv::Mat imgRotation;
-		cv::Mat imgRotationBackward;
-		cv::Mat EpipoleLeftLast;
-		cv::Mat EpipoleLeft;
-		cv::Mat translationLeftLast;
-		cv::Mat translationLeft;
-		cv::Mat fundamentalMatrix;
-		cv::Mat derivativeFlowLeftLast;
-		cv::Mat derivativeFlowLeft;
-		cv::Mat disFlag;
-		cv::Mat disFlagBackward;
-		virtual void createDisparity(cv::Mat &disparity);
-		virtual void computeDerivative();
-		virtual void computeCost();
-		virtual void computeCostRight();
-		virtual void postProcess(cv::Mat &disparity);
-		void computeRotation();
-		void computeBackwardRotation();
-		void computeTranslation(cv::Mat &translation, cv::Mat &Epipole, float sign);
-		virtual void resetDirAccumulatedCost();
-		virtual void resetDirAccumulatedCostLast();
-		virtual void consistencyCheck(cv::Mat disparityLeft, cv::Mat disparityRight, cv::Mat disparity, bool interpl);
-	public:
-		SGMFlow(const cv::Mat &imgLeftLast_, const cv::Mat &imgLeft_, const cv::Mat &imgRight_, const int PENALTY1_, const int PENALTY2_, const int winRadius_, 
-			cv::Mat &EpipoleLeftLast_, cv::Mat &EpipoleLeft_, cv::Mat &fundamentalMatrix_);
-		virtual ~SGMFlow();
-		virtual void writeDerivative();
-		void copyDisflag(cv::Mat &M);
-		void computeFlow(cv::Mat &disparity, std::string fileName);
+public:
+  SGMFlow(const cv::Mat &imgLeftLast, const cv::Mat &imgLeft, const cv::Mat &imgRight, parameter_type& parameters,
+        cv::Mat &EpipoleLeftLast_, cv::Mat &EpipoleLeft_, cv::Mat &fundamentalMatrix_);
+  virtual ~SGMFlow();
+  void computeFlow(cv::Mat &disparity, std::string fileName);
 };
-
-
-
-
-#endif
+}
